e24ec1f5f383028bad230357fcc4e147a0028a1c
Leonardo Pedro <leonardo.cienciacomputer@gmail.com>
2019-08-08 16:27:46 -0300

    Aula 4.1 - Isolando a responsabilidade de conversão de datas

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -11,21 +11,12 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
 
-    let data = new Date(
-      //acessando somente o mês
-      ...this._inputData.value
-        .split("-")
-        .map((item, indice) => item - (indice % 2))
-    );
+    let helper = new DateHelper()
 
     let negociacao = new Negociacao(
       data,
       this._inputQuantidade.value,
       this._inputValor.value
     );
-
-    let diaMesAno = negociacao.data.getDate() + '/' + (negociacao.data.getMonth() + 1) + '/' + negociacao.data.getFullYear()
-    console.log(diaMesAno)
-
   }
 }
---
+++ js/app/helpers/DateHelper.js
@@ -0,0 +1,13 @@
+class DateHelper {
+
+    dataParaTexto(data) {
+        return data.getDate() + '/' 
+        + (negociacao.data.getMonth() + 1) + '/' 
+        + negociacao.data.getFullYear()
+    }
+
+    textoParaData(texto) {
+        return new Date(...texto.split("-")
+            .map((item, indice) => item - (indice % 2))
+    }    
+}
